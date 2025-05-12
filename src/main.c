18afc5338e4b28c5ce2094221ed36d96fd2de9e6
Joao Danilo Correia Jota <joaodanilo2006@gmail.com>
2025-05-12 17:28:57 -0300

    função addLetterPlayerList pronta

--- src/main.c
+++ src/main.c
@@ -6,16 +6,15 @@ typedef struct Word{
     char letter;
     int position;
     struct Word *next;
-    struct Word *before;
 } Word;
 
 
 // ADICIONA EM LISTA ENCADEADA AS LETRAS DA STRING GERADA POR GEMINI
-void geminiWordGenerator(Word **head, Word **tail){}
+void geminiWordGenerator(Word **head, Word **tail);
 
 // CRIA QUANTIDADE DE NÓS NECESSÁRIAS NA LISTA DO JOGADOR COM CHARS VAZIOS PARA ARMAZENAR ONDE
 // ESTÁ CADA PALAVRA ADIVINHADA PELO JOGADOR.
-void createPlayerList(Word *head_pilha_word, Word **tail_player){}
+void createPlayerList(Word *head_pilha_word, Word **tail_player);
 
 // CHECA SE PALAVRA ADIVINHADA PELO JOGADOR == A DO GEMINI (ESTAO NO MESMO INDEX)
 int checkLetter(Word **head_gemini, Word **head_player){
@@ -24,13 +23,13 @@ int checkLetter(Word **head_gemini, Word **head_player){
 }
 
 // ADICIONAR EM INDEX A PALAVRA ADIVINHADA PELO JOGADOR
-void addLetterPlayerList(Word **head_player, int index){}
+void addLetterPlayerList(Word **head_player, int index, char letter);
 
 // ADICIONA LETRA EM PILHA (DE PALAVRAS ADIVINHADAS POR JOGADOR)
-void addPilha(Word **head_pilha){}
+void addPilha(Word **head_pilha);
 
 // REORGANIZA EM ORDEM ALFABETICA NA PILHA DE PALAVRAS ADICIONADAS PELO JOGADOR
-void bubblesort(Word **head_pilha){}
+void bubblesort(Word **head_pilha);
 
 void startGame();
 
@@ -48,4 +47,15 @@ int main() {
 
     CloseWindow();
     return 0;
+}
+
+
+void addLetterPlayerList(Word **head_player, int index, char letter){
+    Word *aux = *head_player;
+
+    while(aux->position != index){
+        aux= aux->next;
+    }
+
+    aux->letter = letter;
 }
